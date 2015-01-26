495ffd32afac59c22ed4eb853e8ca3d97d646236
longhuawhu <longhua1949hm@163.com>
2015-01-26 14:18:57 +0800

    Initial Commit

---
+++ 手势解锁/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手势解锁
+//
+//  Created by lh on 15/1/26.
+//  Copyright (c) 2015年 lh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 手势解锁/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  手势解锁
+//
+//  Created by lh on 15/1/26.
+//  Copyright (c) 2015年 lh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
